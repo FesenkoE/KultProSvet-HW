3ef8f63a91e113d437e4feca236835a46e4e4ac4
FesenkoE <fesenkoe@ukr.net>
2019-09-02 20:08:39 +0300

    question for show book card 1

--- book_store/src/components/Search.vue
+++ book_store/src/components/Search.vue
@@ -2,7 +2,9 @@
     <div class="container search-block">
         <div class="row">
             <div class="col-md-6">
-                <input type="text" class="form-control" placeholder="Search book">
+                <input type="text"
+                       class="form-control"
+                       placeholder="Search book">
             </div>
             <div class="col-md-2">
                 <button
@@ -13,12 +15,10 @@
             <div class="col-md-2"></div>
             <div class="col-md-2"><i class="fas fa-shopping-cart"></i></div>
         </div>
-        <div v-for="(book, index) in $store.state.books"
-            :book = "book.volumInfo"></div>
-<!--            <div><img :src="book.volumeInfo.imageLinks.smallThumbnail" class="mb-5" style="border: 1px solid gray">-->
-
+        <div v-for="(book, index) in $store.state.books">
+            <img :src="book.volumeInfo.imageLinks.smallThumbnail" class="mb-5" style="border: 1px solid gray">
+        </div>
     </div>
-<!--    </div>-->
 </template>
 
 <script>
--- book_store/src/store.js
+++ book_store/src/store.js
@@ -5,8 +5,8 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
+        title: 'Book Store',
         books: [],
-        title: 'Book Store'
     },
     mutations: {
         initBooks(state, payload) {

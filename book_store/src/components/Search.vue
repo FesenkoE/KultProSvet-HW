272f846e077c7b2aabce2740c1bb036f802e56de
FesenkoE <fesenkoe@ukr.net>
2019-09-02 10:05:41 +0300

    queryParam for search

--- book_store/src/components/Search.vue
+++ book_store/src/components/Search.vue
@@ -2,23 +2,22 @@
     <div class="container search-block">
         <div class="row">
             <div class="col-md-6">
-                <input type="text" class="form-control" placeholder="Search book">
+                <input type="text" class="form-control"
+                       placeholder="Search book"
+                       v-model="value"
+                >
             </div>
             <div class="col-md-2">
                 <button
                         class="btn btn-primary btn-block"
-                        @click="initBooks">Send
+                        @click="initBooks(value)">Send
                 </button>
             </div>
             <div class="col-md-2"></div>
             <div class="col-md-2"><i class="fas fa-shopping-cart"></i></div>
         </div>
-        <div v-for="(book, index) in $store.state.books"
-            :book = "book.volumInfo"></div>
-<!--            <div><img :src="book.volumeInfo.imageLinks.smallThumbnail" class="mb-5" style="border: 1px solid gray">-->
 
     </div>
-<!--    </div>-->
 </template>
 
 <script>
@@ -26,9 +25,15 @@
 
     export default {
         name: "Search",
+        props: ['searchParam'],
         methods:
             mapActions([
                 'initBooks'
-            ])
+            ]),
+        data() {
+            return {
+                value: '',
+            }
+        }
     }
 </script>
